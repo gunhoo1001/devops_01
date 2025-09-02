6071592427a8c88d09288b75180f6a2dffcde92a
gunhoo <gunhoo1001@naver.com>
2025-09-02 09:43:59 +0900

    first commit

---
+++ src/main/java/com/spring/app/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.spring.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+
+	
+}
